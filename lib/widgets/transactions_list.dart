1869b15d981254628a02282b3d967de193a2e692
⛅ Arnab Deka 🍭 <arnab.deka@gmail.com>
2022-09-16 13:57:26 -0700

    use a ListView to scroll

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -11,54 +11,57 @@ class TransactionList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _transactions.map((tx) {
-        return Card(
-          child: Row(
-            children: [
-              Container(
-                margin: const EdgeInsets.symmetric(
-                  vertical: 10,
-                  horizontal: 15,
-                ),
-                padding: const EdgeInsets.all(10),
-                decoration: BoxDecoration(
-                  border: Border.all(
-                    color: Colors.purple,
-                    width: 2,
-                  ),
-                ),
-                child: Text(
-                  Money.fromNum(tx.amount, code: 'CAD').format('S0.00'),
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
-                  ),
-                ),
-              ),
-              Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+    return SizedBox(
+      height: 500,
+      child: ListView(
+          children: _transactions.map((tx) {
+            return Card(
+              child: Row(
                 children: [
-                  Text(
-                    tx.title,
-                    style: const TextStyle(
-                      fontSize: 16,
-                      fontWeight: FontWeight.bold,
+                  Container(
+                    margin: const EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 15,
+                    ),
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
+                    ),
+                    child: Text(
+                      Money.fromNum(tx.amount, code: 'CAD').format('S0.00'),
+                      style: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                        color: Colors.purple,
+                      ),
                     ),
                   ),
-                  Text(
-                    DateFormat.yMMMEd().format(tx.createdAt),
-                    style: const TextStyle(
-                      color: Colors.grey,
-                    ),
-                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        tx.title,
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        DateFormat.yMMMEd().format(tx.createdAt),
+                        style: const TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ],
+                  )
                 ],
-              )
-            ],
-          ),
-        );
-      }).toList(),
-    );
+              ),
+            );
+          }).toList(),
+        ),
+      );
   }
 }
