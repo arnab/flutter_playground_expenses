c43ce9c87a6d75ac3f970a652dd0ca0e16238711
⛅ Arnab Deka 🍭 <arnab.deka@gmail.com>
2022-09-23 12:06:57 -0700

    show extra information with TextButton.icon if there is extra width

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -18,78 +18,74 @@ class TransactionList extends StatelessWidget {
       height: 700,
       child: _transactions.isEmpty
           ? LayoutBuilder(builder: (ctx, constraints) {
-        return Column(
-          children: [
-            Text(
-              'Please add some transactions!',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .titleMedium,
-            ),
-            const SizedBox(height: 25),
-            SizedBox(
-              height: constraints.maxHeight * 0.6,
-              child: Image.asset(
-                'assets/images/waiting.png',
-                fit: BoxFit.cover,
-              ),
-            ),
-          ],
-        );
-      }) : ListView.builder(
-        itemCount: _transactions.length,
-        itemBuilder: (ctx, i) {
-          final tx = _transactions[i];
-          return Card(
-            elevation: 5,
-            margin: const EdgeInsets.symmetric(
-              horizontal: 5,
-              vertical: 8,
-            ),
-            child: ListTile(
-              leading: CircleAvatar(
-                radius: 30,
-                backgroundColor: Theme
-                    .of(context)
-                    .colorScheme
-                    .primary,
-                foregroundColor:
-                Theme
-                    .of(context)
-                    .colorScheme
-                    .inversePrimary,
-                child: Padding(
-                  padding: const EdgeInsets.all(6),
-                  child: FittedBox(
-                    child: Text(
-                      Money.fromNum(tx.amount, code: 'CAD').format('S0.00'),
+              return Column(
+                children: [
+                  Text(
+                    'Please add some transactions!',
+                    style: Theme.of(context).textTheme.titleMedium,
+                  ),
+                  const SizedBox(height: 25),
+                  SizedBox(
+                    height: constraints.maxHeight * 0.6,
+                    child: Image.asset(
+                      'assets/images/waiting.png',
+                      fit: BoxFit.cover,
                     ),
                   ),
-                ),
-              ),
-              title: Text(
-                tx.title,
-                style: Theme
-                    .of(context)
-                    .textTheme
-                    .titleMedium,
-              ),
-              subtitle: Text(
-                DateFormat.yMMMEd().format(tx.createdAt),
-              ),
-              trailing: IconButton(
-                icon: const Icon(Icons.delete),
-                color: Theme
-                    .of(context)
-                    .colorScheme
-                    .error,
-                onPressed: () => _deleteTransaction(tx.id),
-              ),
+                ],
+              );
+            })
+          : ListView.builder(
+              itemCount: _transactions.length,
+              itemBuilder: (ctx, i) {
+                final tx = _transactions[i];
+                return Card(
+                  elevation: 5,
+                  margin: const EdgeInsets.symmetric(
+                    horizontal: 5,
+                    vertical: 8,
+                  ),
+                  child: ListTile(
+                    leading: CircleAvatar(
+                      radius: 30,
+                      backgroundColor: Theme.of(context).colorScheme.primary,
+                      foregroundColor:
+                          Theme.of(context).colorScheme.inversePrimary,
+                      child: Padding(
+                        padding: const EdgeInsets.all(6),
+                        child: FittedBox(
+                          child: Text(
+                            Money.fromNum(tx.amount, code: 'CAD')
+                                .format('S0.00'),
+                          ),
+                        ),
+                      ),
+                    ),
+                    title: Text(
+                      tx.title,
+                      style: Theme.of(context).textTheme.titleMedium,
+                    ),
+                    subtitle: Text(
+                      DateFormat.yMMMEd().format(tx.createdAt),
+                    ),
+                    trailing: MediaQuery.of(context).size.width > 400
+                        ? TextButton.icon(
+                            icon: const Icon(Icons.delete),
+                            style: TextButton.styleFrom(
+                              foregroundColor: Theme.of(context).colorScheme.error
+                            ),
+                            label: const Text('Delete'),
+                            onPressed: () => _deleteTransaction(tx.id),
+                          )
+                        : IconButton(
+                            icon: const Icon(Icons.delete),
+                            color: Theme.of(context).colorScheme.error,
+                            onPressed: () => _deleteTransaction(tx.id),
+                          ),
+                  ),
+                );
+              },
             ),
-          );
-        },
-      ),
     );
   }
 }
